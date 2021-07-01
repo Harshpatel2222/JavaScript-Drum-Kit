e8fd69735a3dd36216ed188d39e525ad6d732728
Harsh Patel <61933209+Harshpatel2222@users.noreply.github.com>
2021-07-01 19:49:01 +0530

    app.js added

---
+++ app.js
@@ -0,0 +1,7 @@
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.play();
+    console.log(key);
+});
