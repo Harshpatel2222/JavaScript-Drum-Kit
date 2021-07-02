365b7ac481229bd6e66500fb19e9f5b1396ad4ba
Harsh Patel <61933209+Harshpatel2222@users.noreply.github.com>
2021-07-02 09:29:53 +0530

    Transition added

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-window.addEventListener('keydown', function(e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
+
+    key.classList.add('playing')
+    audio.currentTime = 0;
     audio.play();
-    console.log(key);
-});
+    // console.log(key);
+}
+
+window.addEventListener('keydown', playSound)
+
+function removeTransition(e) {
+    if (e.propertyName != 'transform') return;
+
+    e.target.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
